50e3f7a8a4fe5edf200e6958605c3903c68f8ef6
Esp1ritus <antoshko.v@bk.ru>
2025-09-30 13:00:03 +0300

    Добавил текстовое поле, там даже можно печатать!

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+window = Tk()
 
+window.geometry("300x300")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+text = Text(window)
 
+text.pack()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop()
